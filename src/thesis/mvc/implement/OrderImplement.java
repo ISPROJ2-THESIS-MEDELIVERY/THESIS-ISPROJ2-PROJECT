ed18ce335a08e4666c965faa2d37e38a0f7586fc
Insightguy <Insightguy@users.noreply.github.com>
2018-02-13 11:07:20 +0800

    zfsdf
    sdfds

--- src/thesis/mvc/implement/OrderImplement.java
+++ src/thesis/mvc/implement/OrderImplement.java
@@ -2,6 +2,10 @@ package thesis.mvc.implement;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import thesis.mvc.dataobjects.OrderDAO;
@@ -44,26 +48,108 @@ public class OrderImplement implements OrderDAO{
 
 	@Override
 	public void deleteOrder(int orderId) {
-		// TODO Auto-generated method stub
+		try {
+			String query = "";
+			PreparedStatement preparedStatement = conn.prepareStatement( query );
+			preparedStatement.setInt(1, orderId);
+			preparedStatement.executeUpdate();
+			preparedStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
 	public void updateOrder(Order order) {
-		// TODO Auto-generated method stub
+		try {
+			Date CurrentDate = new Date(Calendar.getInstance().getTime().getTime());
+			String query = "";
+			PreparedStatement preparedStatement = conn.prepareStatement( query );
+			preparedStatement.setInt( 1, order.getCustomerID() );
+			preparedStatement.setInt( 2, order.getDeliveryID() );
+			preparedStatement.setInt( 3, order.getPharmacistID() );
+			preparedStatement.setInt( 4, order.getCityID() );
+			preparedStatement.setInt( 5, order.getPrescriptionID() );
+			preparedStatement.setString( 6, order.getOrderAddress() );
+			preparedStatement.setDate( 7, order.getDateOrdered() );
+			preparedStatement.setDate( 8, order.getDateProcessed() );
+			preparedStatement.setDate( 9, order.getDateDelivered() );
+			preparedStatement.setString( 10, order.getOrderType() );
+			preparedStatement.setString( 11, order.getOrderStatus() );
+			preparedStatement.setBoolean( 12, order.getSeniorDiscount() );
+			preparedStatement.setString( 13, order.getPaymentMethod() );
+			preparedStatement.setInt( 14, order.getOrderID() );
+			preparedStatement.executeUpdate();
+			preparedStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
 		
 	}
 
 	@Override
 	public List<Order> getOrder() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Order> orders = new ArrayList<Order>();
+		try {
+			Statement statement = conn.createStatement();
+			ResultSet resultSet = statement.executeQuery( "" );
+			while( resultSet.next() ) {
+				Order order = new Order();
+				order.setOrderID( resultSet.getInt( "OrderID" ) );
+				order.setCustomerID( resultSet.getInt( "CustomerID" ) );
+				order.setDeliveryID( resultSet.getInt( "DeliveryID" ) );
+				order.setPharmacistID( resultSet.getInt( "PharmacistID" ) );
+				order.setCityID( resultSet.getInt( "CityID" ) );
+				order.setPrescriptionID( resultSet.getInt( "PrescriptionID" ) );
+				order.setOrderAddress( resultSet.getString( "OrderAddress" ) );
+				order.setDateOrdered( resultSet.getDate( "DateOrdered" ) );
+				order.setDateProcessed( resultSet.getDate( "DateProcessed" ) );
+				order.setDateDelivered( resultSet.getDate( "DateDelivered" ) );
+				order.setOrderType( resultSet.getString( "OrderType" ) );
+				order.setOrderStatus( resultSet.getString( "OrderStatus" ) );
+				order.setSeniorDiscount( resultSet.getBoolean( "SeniorDiscount" ) );
+				order.setPaymentMethod( resultSet.getString( "PaymentMethod" ) );
+				orders.add(order);
+			}
+			resultSet.close();
+			statement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return orders;
 	}
 
 	@Override
 	public Order getOrderById(int orderId) {
-		// TODO Auto-generated method stub
-		return null;
+		Order order = new Order();
+		try {
+			String query = "";
+			PreparedStatement preparedStatement = conn.prepareStatement( query );
+			preparedStatement.setInt(1, orderId);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while( resultSet.next() ) {
+				order.setOrderID( resultSet.getInt( "OrderID" ) );
+				order.setCustomerID( resultSet.getInt( "CustomerID" ) );
+				order.setDeliveryID( resultSet.getInt( "DeliveryID" ) );
+				order.setPharmacistID( resultSet.getInt( "PharmacistID" ) );
+				order.setCityID( resultSet.getInt( "CityID" ) );
+				order.setPrescriptionID( resultSet.getInt( "PrescriptionID" ) );
+				order.setOrderAddress( resultSet.getString( "OrderAddress" ) );
+				order.setDateOrdered( resultSet.getDate( "DateOrdered" ) );
+				order.setDateProcessed( resultSet.getDate( "DateProcessed" ) );
+				order.setDateDelivered( resultSet.getDate( "DateDelivered" ) );
+				order.setOrderType( resultSet.getString( "OrderType" ) );
+				order.setOrderStatus( resultSet.getString( "OrderStatus" ) );
+				order.setSeniorDiscount( resultSet.getBoolean( "SeniorDiscount" ) );
+				order.setPaymentMethod( resultSet.getString( "PaymentMethod" ) );
+			}
+			resultSet.close();
+			preparedStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return order;
 	}
 
 }

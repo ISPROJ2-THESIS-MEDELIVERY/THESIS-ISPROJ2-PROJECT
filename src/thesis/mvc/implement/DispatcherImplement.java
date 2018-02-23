0f751e0cfa086d58fef93eee846ddb7d6dbea1f9
AlbertPhilippeCalvelo <35923405+AlbertPhilippeCalvelo@users.noreply.github.com>
2018-02-23 12:42:40 +0800

    audit2

--- src/thesis/mvc/implement/DispatcherImplement.java
+++ src/thesis/mvc/implement/DispatcherImplement.java
@@ -1,11 +1,13 @@
 package thesis.mvc.implement;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import thesis.mvc.dataobjects.DispatcherDAO;
@@ -33,6 +35,18 @@ public class DispatcherImplement implements DispatcherDAO{
 			preparedStatement.setInt( 5, dispatcher.getContactNumber() );
 			preparedStatement.setString( 6, dispatcher.getAddress() );
 			preparedStatement.setDate( 7, dispatcher.getBirthdate() );
+			preparedStatement.executeUpdate();
+			preparedStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} try {
+			Date CurrentDate = new Date(Calendar.getInstance().getTime().getTime());
+			String query = "INSERT INTO Audit (UserID, LogType, Timestamp, ActionTaken) VALUES (?,?,?,?)";
+			PreparedStatement preparedStatement = conn.prepareStatement( query );
+			preparedStatement.setInt( 1, 3 ); //3 is a placeholder
+			preparedStatement.setString( 2, "create, update or delete" ); //placeholder
+			preparedStatement.setDate( 3, CurrentDate );
+			preparedStatement.setString( 4, "User with ID " + dispatcher.getUserID() );
 			preparedStatement.executeUpdate();
 			preparedStatement.close();
 		} catch (SQLException e) {
--- src/thesis/mvc/implement/DriverImplement.java
+++ src/thesis/mvc/implement/DriverImplement.java
@@ -1,11 +1,13 @@
 package thesis.mvc.implement;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import thesis.mvc.dataobjects.DriverDAO;
@@ -28,6 +30,18 @@ public class DriverImplement implements DriverDAO {
 			preparedStatement.setString( 1, driver.getDriverName() );
 			preparedStatement.setString( 2, driver.getDriverAddress() );
 			preparedStatement.setString( 3, driver.getDriverContactNumber() );
+			preparedStatement.executeUpdate();
+			preparedStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} try {
+			Date CurrentDate = new Date(Calendar.getInstance().getTime().getTime());
+			String query = "INSERT INTO Audit (UserID, LogType, Timestamp, ActionTaken) VALUES (?,?,?,?)";
+			PreparedStatement preparedStatement = conn.prepareStatement( query );
+			preparedStatement.setInt( 1, 3 ); //3 is a placeholder
+			preparedStatement.setString( 2, "create, update or delete" ); //placeholder
+			preparedStatement.setDate( 3, CurrentDate );
+			preparedStatement.setString( 4, "User with ID " + "" );
 			preparedStatement.executeUpdate();
 			preparedStatement.close();
 		} catch (SQLException e) {

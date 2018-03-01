5b516234d219011172fc85b6aa963224760356e6
AlbertPhilippeCalvelo <35923405+AlbertPhilippeCalvelo@users.noreply.github.com>
2018-03-01 16:46:34 +0800

    Purchase Action (probably needs changes)

--- src/thesis/mvc/pageaction/PurchaseAction.java
+++ src/thesis/mvc/pageaction/PurchaseAction.java
@@ -18,7 +18,7 @@ public class PurchaseAction {
 		conn = DBUtility.getConnection();
 	}
 	
-	public void purchaseOrder(Order order, List<OrderDetail> OrderDetails, int CustomerID, String BranchProvince) {
+	public void purchaseOrder(Order order, List<OrderDetail> OrderDetails, int CustomerID, int PharmacistID, int BranchID) {
 		int Deliverycharge = 0;
 		int CityCustomer = -2;
 		int CityPharmacy = -1;
@@ -35,8 +35,23 @@ public class PurchaseAction {
 		int PharmaID = order.getPharmacistID();
 		
 		//What branch ID does the pharmacist come from
-		
+		try(PreparedStatement stmt = conn.prepareStatement("SELECT * FROM pharmacist WHERE PharmacistID = ?")) {
+			stmt.setInt(1, PharmacistID);
+			try(ResultSet rs = stmt.executeQuery()) {
+				PharmaID = rs.getInt("BranchID");
+			}
+		} catch (SQLException e){
+			e.printStackTrace();
+		}
 		//Where is the branch located.
+		try(PreparedStatement stmt = conn.prepareStatement("SELECT * FROM branch WHERE BranchID = ?")) {
+			stmt.setInt(1, BranchID);
+			try(ResultSet rs = stmt.executeQuery()){
+				CityPharmacy = rs.getInt("CityID");
+			}
+		} catch (SQLException e){
+			e.printStackTrace();
+		}
 		
 		
 		if (CityCustomer == CityPharmacy) {

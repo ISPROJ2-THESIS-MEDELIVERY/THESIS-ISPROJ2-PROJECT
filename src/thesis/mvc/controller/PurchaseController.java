6270fda0e415b887132d2f2520e6d36ab0047c2a
Insightguy <Insightguy@users.noreply.github.com>
2018-06-21 22:36:03 +0800

    test

--- src/thesis/mvc/controller/PurchaseController.java
+++ src/thesis/mvc/controller/PurchaseController.java
@@ -91,6 +91,7 @@ public class PurchaseController extends HttpServlet {
 				boolean SID = customer.isIsSeniorCitizen();
 				int CID = customer.getCityID();
 				
+				//HELLO PEOPLE
 				//order.setSeniorDiscount( session.getAttribute("seniorStatus") );
 				order = purchaseAction.setInitalOrder(UID, ADD, SID, CID);
 				session.setAttribute("Order", order );

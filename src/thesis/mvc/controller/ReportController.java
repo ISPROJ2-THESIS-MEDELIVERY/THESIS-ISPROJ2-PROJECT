221d2d8f2cf69a382bedef9064730eaf2f2abbf2
Insightguy <Insightguy@users.noreply.github.com>
2018-06-22 11:34:19 +0800

    setup for the storepage

--- src/thesis/mvc/controller/ReportController.java
+++ src/thesis/mvc/controller/ReportController.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import thesis.mvc.pageaction.ApprovalAction;
+import thesis.mvc.pageaction.FilterAction;
 import thesis.mvc.pageaction.PurchaseAction;
 import thesis.mvc.utility.DBUtility;
 
@@ -27,33 +28,22 @@ public class ReportController {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String forward;
-		String action = request.getParameter( "action" );
+		int BranchID = Integer.parseInt( request.getParameter( "BranchID" ) );
+		String Pharma = request.getParameter( "BranchID" );
 		HttpSession session = request.getSession();
-		ApprovalAction approvalAction = new ApprovalAction();
+		forward = "/Catalog.jsp";
 		
-    	if (action.equalsIgnoreCase("Customer")) {
-    		forward = "/A-test-shop.jsp";
-    		request.setAttribute( "productList", approvalAction.getProducts() );
-    	} else if (action.equalsIgnoreCase("Pharmacist")) {
-    		forward = "A-test-pharmacistapproval.jsp";
-    		session.setAttribute("orderPharmacistCheck", approvalAction.getOrder() );
-    	} else if (action.equalsIgnoreCase("Approve")) {
-    		forward = "A-test-pharmacistapprovalsuccess.jsp";
-    		PurchaseAction purchaseAction = new PurchaseAction();
-    		purchaseAction.pharmacistApproval( Integer.parseInt( request.getParameter( "orderID" ) ), true );
-    		
-    	} else if (action.equalsIgnoreCase("Reject")) {
-    		forward = "A-test-pharmacistapprovalsuccess.jsp";
-    		PurchaseAction purchaseAction = new PurchaseAction();
-    		purchaseAction.pharmacistApproval( Integer.parseInt( request.getParameter( "orderID" ) ) , false );
-    		
-    	} else {
-    		forward = "";
-    	}
+		String Test = "SELECT product.ProductName, product.GenericName, product.ProductStrength, product.ProductForm, product.ProductPackaging, product.ProductDescription, product.ProductImage, stocks.Quantity, stocksprice.PriceSet FROM stocks INNER JOIN stocksprice ON stocksprice.StockID = stocks.StockID INNER JOIN product ON stocks.ProductID = product.ProductID WHERE stocksprice.IsCurrent = 1 AND stocks.BranchID = ?";
+		
 		RequestDispatcher view = request.getRequestDispatcher( forward );
 		view.forward(request, response);
 		
 	}
+	
+
+	//FilterAction filteraction = new FilterAction();
+	//request.setAttribute( "SearchList", filteraction.ProductListing(1) );
+	//session.setAttribute("orderPharmacistCheck", approvalAction.getOrder() );
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		

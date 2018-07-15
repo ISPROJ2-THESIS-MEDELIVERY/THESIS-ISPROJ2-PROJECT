b30e2f9924fd684d24e72d8632721de8f9ddd75c
Insightguy <Insightguy@users.noreply.github.com>
2018-07-15 14:48:56 +0800

    start of a couple of changes

--- src/thesis/mvc/controller/HomeController.java
+++ src/thesis/mvc/controller/HomeController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpSession;
 import thesis.mvc.implement.BranchImplement;
 import thesis.mvc.implement.CustomerImplement;
 import thesis.mvc.implement.OrderImplement;
+import thesis.mvc.implement.PharmacistImplement;
 
 @WebServlet("/HomeController")
 public class HomeController  extends HttpServlet {
@@ -21,13 +22,16 @@ public class HomeController  extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		BranchImplement branchImplement = new BranchImplement();
-		OrderImplement orderImplement = new OrderImplement();
-		request.setAttribute( "Pharmacy", branchImplement.getAllBranch());
-		if (session.getAttribute("userID") != null) {
-			CustomerImplement customerImplement = new CustomerImplement();
-			int test = customerImplement.getCustomerByUserId((int) session.getAttribute("userID")).getCustomerID();
-			request.setAttribute( "PurchasePending", orderImplement.getPendingOrder(test)) ;
+		int AL = 0;
+		switch(AL) {
+			case 1:
+				PharmacistImplement pharmacistImplement = new PharmacistImplement();
+				OrderImplement orderImplement = new OrderImplement();
+				request.setAttribute( "PurchasePending", orderImplement.getPendingOrder((int) session.getAttribute("CustomerID"))) ;0
+				//Remember to add a report button
+				break;
+			default:
+				break;
 		}
 		RequestDispatcher view = request.getRequestDispatcher( "/Home.jsp" );
 		view.forward(request, response);
